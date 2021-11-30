8052462d307edafd25d5bb71b9b5fbe8c8fa0995
ProfEvans15 <ne.nathanevans15@gmail.com>
2021-11-30 13:24:20 -0600

    Update to UI

---
+++ app/src/main/java/com/example/wsrconnect/MyService.java
@@ -0,0 +1,16 @@
+package com.example.wsrconnect;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyService extends FirebaseMessagingService {
+    public MyService() {
+    }
+
+
+}
