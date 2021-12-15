4bd62bc45512dce76a5535cd3df954f7f6bfd152
ProfEvans15 <ne.nathanevans15@gmail.com>
2021-12-15 13:14:43 -0600

    Working Scroll

--- app/src/main/java/com/example/wsrconnect/MainActivity.java
+++ app/src/main/java/com/example/wsrconnect/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.wsrconnect;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +10,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        InitText();
+
+    }
+    //Run on start builds the MAIN structure list of past events
+    public void InitText(){
+        TextView textView = findViewById(R.id.RunningLog);
+        String newText = "";
+        StringBuilder text = new StringBuilder();
+        for(int i=0; i<=100; i++) {
+            text.append(newText).append(i).append("\n");
+        }
+        textView.setText(text);
     }
 }
